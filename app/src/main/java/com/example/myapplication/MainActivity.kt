11c5c0a06c88b9c72111501c312c63a899180f66
John Doe <funkdarya@gmail.com>
2019-10-25 10:12:57 +0700

    tadaaaaaaaam

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -214,23 +214,25 @@ class Game(val p1:AiTicTacToe, val p2 : AiTicTacToe){
     var turn: Int = 0
 
     fun round(){
-
-        var t : Point = p1.evaluation(this)
-        this.pole[t.x][t.y] = turn % 2 + 1
-        turn++
+        if (!check())
+        {
+            var t : Point = p1.evaluation(this)
+            this.pole[t.x][t.y] = turn % 2 + 1
+            turn++
+        }
     }
 
     fun roundUser(t: PointF){
-        if (this.pole[t.x.toInt()][t.y.toInt()] == 0)
+        if (this.pole[t.x.toInt()][t.y.toInt()] == 0 && (!check()))
         {
             this.pole[t.x.toInt()][t.y.toInt()] = turn % 2 + 1
             turn++
             output() ///////////////////////////////////////////////////////////////////////////////
 
-            if (this.check()) {Log.d("CONTROLLA","ПОБЕДИЛ ИГРОК \n"); while (true) {}}
+            if (this.check()) {Log.d("CONTROLLA","ПОБЕДИЛ ИГРОК \n"); while (false) {}}
             round() ////////////////////////////////////////////////////////////////////////////////
             output()
-            if (this.check()) {Log.d("CONTROLLA","ПОБЕДИЛ КОМПУДАКТЕР \n"); while (true) {}}
+            if (this.check()) {Log.d("CONTROLLA","ПОБЕДИЛ КОМПУДАКТЕР \n"); while (false) {}}
         }
     }
 
@@ -336,19 +338,113 @@ class Game(val p1:AiTicTacToe, val p2 : AiTicTacToe){
         }
 
 
-        var x:Int = 2
-        var y:Int = 3
 
-        brus.color = Color.BLUE  //BLUE O
-        canvas.drawCircle(x.toFloat() * (canvas.width / psize) + canvas.width / psize / 2  ,y.toFloat() * (canvas.height / psize) + canvas.height / psize / 2, (canvas.width / psize / 2).toFloat(), brus)
+        for (x in 0..psize-1)
+        {
+            for (y in 0..psize-1)
+            {
+                if (pole[x][y] == 1)
+                {
+                    brus.color = Color.BLUE  //BLUE O
+                    canvas.drawCircle(y.toFloat() * (canvas.width / psize) + canvas.width / psize / 2  ,x.toFloat() * (canvas.height / psize) + canvas.height / psize / 2, (canvas.width / psize / 2).toFloat(), brus)
+                }
+                else if (pole[x][y] == 2)
+                {
+                    brus.color = Color.RED  //RED X
+                    canvas.drawLine(y.toFloat() * (canvas.width / psize), x.toFloat() * (canvas.height / psize), (y+1).toFloat() * (canvas.width / psize), (x+1).toFloat() * (canvas.height / psize), brus)
+                    canvas.drawLine((y+1).toFloat() * (canvas.width / psize), x.toFloat() * (canvas.height / psize), y.toFloat() * (canvas.width / psize), (x+1).toFloat() * (canvas.height / psize), brus)
+                }
+            }
+        }
+        checkline(canvas)
+    }
 
-        x = 5
-        y = 7
+    fun checkline(canvas : Canvas) : Boolean
+    { //диагонали
+        for (i in 0..this.psize-this.needToWin) { //столбцы
+            for (l in 0..this.psize-this.needToWin) { //строки
+                for (p in 1..2) { //игроки
+
+                    var v : Int = 0 //верные клетки в линии
+                    for (n in 0..needToWin-1) { //клетки линии
+                        if (pole[i+n][l+n] == p) {v++}
+                    }
+                    if (v == needToWin) {
+                        var brus : Paint = Paint()
+                        brus.color = Color.GREEN  //RED X
+                        brus.strokeWidth = 40f
+                        canvas.drawLine((l.toFloat() + 0.5f) * (canvas.width / psize), (i.toFloat() + 0.5f) * (canvas.height / psize), ((l+needToWin).toFloat() - 0.5f) * (canvas.width / psize), ((i+needToWin).toFloat() - 0.5f) * (canvas.height / psize), brus)
+                        return true
+                    }
+                } // функция победы игрока номер P
+
+            }
+        }
 
 
-        brus.color = Color.RED  //RED X
-        canvas.drawLine(x.toFloat() * (canvas.width / psize), y.toFloat() * (canvas.height / psize), (x+1).toFloat() * (canvas.width / psize), (y+1).toFloat() * (canvas.height / psize), brus)
-        canvas.drawLine((x+1).toFloat() * (canvas.width / psize), y.toFloat() * (canvas.height / psize), x.toFloat() * (canvas.width / psize), (y+1).toFloat() * (canvas.height / psize), brus)
+        //диагонали
+        for (i in 0..this.psize-this.needToWin) { //столбцы
+            for (l in this.needToWin-1..this.psize-1) { //строки
+                for (p in 1..2) { //игроки
+
+                    var v : Int = 0 //верные клетки в линии
+                    for (n in 0..needToWin-1) { //клетки линии
+                        if (pole[i+n][l-n] == p) {v++}
+                    }
+                    if (v == needToWin) {
+                        var brus : Paint = Paint()
+                        brus.color = Color.GREEN  //RED X
+                        brus.strokeWidth = 40f
+                        canvas.drawLine(((l-needToWin).toFloat() + 1.5f) * (canvas.width / psize), ((i+needToWin).toFloat() - 0.5f) * (canvas.height / psize), (l.toFloat() + 0.5f) * (canvas.width / psize), (i.toFloat() + 0.5f) * (canvas.height / psize), brus)
+                        return true
+                    } // функция победы игрока номер P
+
+                }
+            }
+        }
+
+        //вертикали
+        for (i in 0..this.psize-1) { //столбцы
+            for (l in 0..this.psize-this.needToWin) { //строки
+                for (p in 1..2) { //игроки
+
+                    var v : Int = 0 //верные клетки в линии
+                    for (n in 0..needToWin-1) { //клетки линии
+                        if (pole[i][l+n] == p) {v++}
+                    }
+                    if (v == needToWin) {
+                        var brus : Paint = Paint()
+                        brus.color = Color.GREEN  //RED X
+                        brus.strokeWidth = 40f
+                        canvas.drawLine((l.toFloat() + 0.5f) * (canvas.width / psize), (i.toFloat() + 0.5f) * (canvas.height / psize), ((l+needToWin).toFloat() - 0.5f) * (canvas.width / psize), (i.toFloat() + 0.5f) * (canvas.height / psize), brus)
+                        return true
+                    } // функция победы игрока номер P
+
+                }
+            }
+        }
+
+        //горизонтали
+        for (i in 0..this.psize-this.needToWin) { //столбцы
+            for (l in 0..this.psize-1) { //строки
+                for (p in 1..2) { //игроки
+
+                    var v : Int = 0 //верные клетки в линии
+                    for (n in 0..needToWin-1) { //клетки линии
+                        if (pole[i+n][l] == p) {v++}
+                    }
+                    if (v == needToWin) {
+                        var brus : Paint = Paint()
+                        brus.color = Color.GREEN  //RED X
+                        brus.strokeWidth = 40f
+                        canvas.drawLine((l.toFloat() + 0.5f) * (canvas.width / psize), (i.toFloat() + 0.5f) * (canvas.height / psize), (l.toFloat() + 0.5f) * (canvas.width / psize), ((i+needToWin).toFloat() - 0.5f) * (canvas.height / psize), brus)
+                        return true
+                    } // функция победы игрока номер P
+
+                }
+            }
+        }
+        return false
     }
 }
 
